40ccf8b7f6dabb1e03ebcc558c49b32ed71b8e9a
saurav1812 <shorya181299@gmail.com>
2023-04-21 12:05:04 +0530

    Commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,15 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/Map/map.dart';
 
-// void main() {
-//   runApp(const MyApp());
-// }
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+void main() {
   runApp(const MyApp());
 }
+
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp();
+//   runApp(const MyApp());
+// }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
